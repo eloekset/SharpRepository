03758202467dd792c41660d92e4a02d4cde679a7
Eivind Gussiås Løkseth <eloekset@msn.com>
2016-07-24 21:35:12 +0200

    The simplest possible in-memory test.

---
+++ SharpRepository.ManualRuntimeTests/Program.cs
@@ -0,0 +1,25 @@
+﻿using SharpRepository.ManualRuntimeTests.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SharpRepository.ManualRuntimeTests
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            InMemoryRepository.InMemoryRepository<Employee> employeeRepo = new InMemoryRepository.InMemoryRepository<Employee>();
+            employeeRepo.Add(new Employee { EmployeeId = 1, Name = "Adam" });
+            employeeRepo.Add(new Employee { EmployeeId = 2, Name = "Ashley" });
+
+            foreach(var employee in employeeRepo.GetAll())
+            {
+                Console.WriteLine($"Employee {employee.EmployeeId} is called {employee.Name}.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
