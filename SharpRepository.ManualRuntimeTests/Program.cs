54c6f2cde0fb4d7a75acc0d35a0a85e7b768dbf8
Eivind Gussiås Løkseth <eloekset@msn.com>
2016-07-31 11:52:52 +0200

    Manually testing deserialization of SharpRepository configuration. Both array style and object style fail.

--- SharpRepository.ManualRuntimeTests/Program.cs
+++ SharpRepository.ManualRuntimeTests/Program.cs
@@ -1,4 +1,7 @@
-﻿using SharpRepository.ManualRuntimeTests.Model;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SharpRepository.ManualRuntimeTests.Model;
+using SharpRepository.Repository.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,13 +11,56 @@ namespace SharpRepository.ManualRuntimeTests
 {
     public class Program
     {
+        private static IConfigurationRoot Configuration;
+        private static SharpRepositoryConfiguration sharpRepository;
+        private static SharpRepositoryConfiguration sharpRepository2;
+
         public static void Main(string[] args)
+        {
+            LoadConfigurationObjectStyle();
+            //LoadConfigurationArrayStyle();
+        }
+
+        private static void LoadConfigurationObjectStyle()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("objectsharprepositorysettings.json");
+
+            Configuration = builder.Build();
+            sharpRepository = new SharpRepositoryConfiguration();
+            var sharpRepositoryConfigurationSection = Configuration.GetSection("sharpRepository");
+            ConfigurationBinder.Bind(sharpRepositoryConfigurationSection, sharpRepository);
+            sharpRepository2 = new SharpRepositoryConfiguration();
+            var sharpRepository2ConfigurationSection = Configuration.GetSection("sharpRepository2");
+            ConfigurationBinder.Bind(sharpRepository2ConfigurationSection, sharpRepository2);
+        }
+
+        private static void LoadConfigurationArrayStyle()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("arraysharprepositorysettings.json");
+
+            Configuration = builder.Build();
+            sharpRepository = new SharpRepositoryConfiguration();
+            var sharpRepositoryConfigurationSection = Configuration.GetSection("sharpRepository");
+            ConfigurationBinder.Bind(sharpRepositoryConfigurationSection, sharpRepository);
+            sharpRepository2 = new SharpRepositoryConfiguration();
+            var sharpRepository2ConfigurationSection = Configuration.GetSection("sharpRepository2");
+            ConfigurationBinder.Bind(sharpRepository2ConfigurationSection, sharpRepository2);
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // TODO: Dig into ASP.NET Core to see how this method gets called.
+        }
+
+        private static void RunSimpleInMemoryTest()
         {
             InMemoryRepository.InMemoryRepository<Employee> employeeRepo = new InMemoryRepository.InMemoryRepository<Employee>();
             employeeRepo.Add(new Employee { EmployeeId = 1, Name = "Adam" });
             employeeRepo.Add(new Employee { EmployeeId = 2, Name = "Ashley" });
 
-            foreach(var employee in employeeRepo.GetAll())
+            foreach (var employee in employeeRepo.GetAll())
             {
                 Console.WriteLine($"Employee {employee.EmployeeId} is called {employee.Name}.");
             }
